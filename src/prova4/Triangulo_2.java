5a2967083c70617e8dbe6d1c1e1ec3fe65b1315e
morp <morp@morp-PC>
2017-08-18 21:10:37 -0300

    Triangulo2 completa

--- src/prova4/Triangulo_2.java
+++ src/prova4/Triangulo_2.java
@@ -11,27 +11,22 @@ public class Triangulo_2 {
 
 	Scanner input = new Scanner(System.in);
 	
-	System.out.println("Digite um numero");
+	System.out.println("Digite um numero: ");
 	num = input.nextInt();
-	System.out.println("Digite o segundo numero");
+	System.out.println("Digite o segundo numero: ");
 	num1 = input.nextInt();
-	System.out.println("Digite o terceiro numero");
+	System.out.println("Digite o terceiro numero: ");
 	num2 = input.nextInt();
 	
-	if (num == num1 && num1 == num2) {
-		
-		System.out.println("**Triângulo Equilátero**");
-		
-	}
-	if (num == num1 && num1 !=num2 ) {
-		
-		System.out.println("**Triângulo Isósceles**");	
-		
-	}
-	if (num != num1 && num1 != num2) {
-		
-		System.out.println("**Triângulo Escaleno**");
-		
+	if ( (num == num1) && (num1 == num2) )  
+	{
+		System.out.println("\n**Triangulo Equilatero**");
+	}else if (num == num1)
+	{
+		System.out.println("\n**Triangulo  isósceles**");		
+	}else
+	{
+		System.out.println("\n**Triangulo  Escaleno**");
 	}
 	
 	
